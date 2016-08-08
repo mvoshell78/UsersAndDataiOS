bdea999e57718a41aa5c130732fe5915c23b4d2f
mvoshell78 <michael.voshell@yahoo.com>
2016-08-08 08:44:50 -0400

    Initial Commit

---
+++ UsersAndDataiOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UsersAndDataiOS
+//
+//  Created by Michael Voshell on 8/8/16.
+//  Copyright © 2016 Michael Voshell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
